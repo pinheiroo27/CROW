701d360b95097866824595222d6e8e8c69ad844f
Gleydson <Gleydson@DESKTOP-LJ24PLU>
2016-10-24 21:53:26 -0300

    Adicionando arquivo do CROW ao repositório

---
+++ CROW/src/main/java/br/edu/garanhuns/ifpe/crow/interfaces/CrowActionController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.garanhuns.ifpe.crow.interfaces;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ *
+ * @author Gleydson
+ */
+public interface CrowActionController<Bean, ID extends Serializable> {
+
+    public void create(Bean bean);
+
+    public Collection<Bean> read();
+
+    public Bean update(Bean entity);
+
+    public void delete(Bean bean);
+
+    public Bean getById(ID id);
+
+}

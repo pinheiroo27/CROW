3d92379235298235809902d58b9dccd59e339489
Anderson Pinheiro <andersonpinheiro@outlook.com>
2016-10-27 19:51:22 -0300

    Método edit jquery

---
+++ CROW/src/main/resources/META-INF/EditMethod.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function(){
+    
+    $("value='editar'").click(function(){
+       var fields = $("[name]");
+    
+        var parametros = "";
+    
+        for(var i = 0;i<fields.length();i++){
+            parametros += $(fields[i]).attr("name")+":"+$(fields[i]).val()+";"; 
+        }
+    
+        $.post("GenericEditServlet",parametros,
+        function(data){
+            $("#mensagem").html(data);
+        }); 
+        });
+    
+    
+    
+});
